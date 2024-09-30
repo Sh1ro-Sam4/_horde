23480d9722d39df371baa4696c5c97f3b1efac9a
root <root@vds-97086.myarena.ru>
2024-09-30 20:32:30 +0000

    Добавил спам в чат

--- addons/shiz-addons/lua/shizlib-addons/chat/network_sh.lua
+++ addons/shiz-addons/lua/shizlib-addons/chat/network_sh.lua
@@ -18,6 +18,10 @@ if SERVER then
         net.WriteString(msg)
         net.Send(self)
     end
+
+    timer.Create('Broadcast-SelfProviding:)', 180, 0, function()
+        shizlib.Broadcast(Color(174,0,197), '[Kroject] ', 'Join us Discord server: https://discord.gg/a8H9eUTcgR')
+    end)
 else
     net.Receive('shizlib-Chat', function(len)
         chat.AddText(net.ReadColor(), net.ReadString(), color_white, net.ReadString())
