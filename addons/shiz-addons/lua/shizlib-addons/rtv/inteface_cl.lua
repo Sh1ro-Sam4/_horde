8787d49e65464bc09f3feef79a08653e4a86a1c6
root <root@vds-97086.myarena.ru>
2024-09-30 20:28:16 +0000

    Продолжаем писать новое RTV меню

--- addons/shiz-addons/lua/shizlib-addons/rtv/inteface_cl.lua
+++ addons/shiz-addons/lua/shizlib-addons/rtv/inteface_cl.lua
@@ -4,91 +4,88 @@ local colors = CFG.skinColors
 
 RTV = RTV or {}
 
-net.Receive('Horde_GameEnd', function()
-    local status = 'DEFEAT'
+-- net.Receive('Horde_GameEnd', function()
+--     local status = 'DEFEAT'
 
-    local mvp = player.GetAll()[1]
-    local mvp_damage = 0
-    local mvp_kills = 0
+--     local mvp = player.GetAll()[1]
+--     local mvp_damage = 0
+--     local mvp_kills = 0
 
-    local damage_player = player.GetAll()[1]
-    local most_damage = 0
+--     local damage_player = player.GetAll()[1]
+--     local most_damage = 0
 
-    local kills_player = player.GetAll()[1]
-    local most_kills = 0
+--     local kills_player = player.GetAll()[1]
+--     local most_kills = 0
 
-    local most_heal_player = player.GetAll()[1]
-    local most_heal = 0
+--     local most_heal_player = player.GetAll()[1]
+--     local most_heal = 0
 
-    local headshot_player = player.GetAll()[1]
-    local most_headshots = 0
+--     local headshot_player = player.GetAll()[1]
+--     local most_headshots = 0
 
-    local elite_kill_player = player.GetAll()[1]
-    local most_elite_kills = 0
+--     local elite_kill_player = player.GetAll()[1]
+--     local most_elite_kills = 0
 
-    local damage_taken_player = player.GetAll()[1]
-    local most_damage_taken = 0
+--     local damage_taken_player = player.GetAll()[1]
+--     local most_damage_taken = 0
 
-    local total_damage = 0
+--     local total_damage = 0
 
-    local maps = {
-        'hr_desolate',
-        'de_stad',
-    }
+--     local maps = {
+--         'hr_desolate',
+--         'de_stad',
+--     }
 
-    -- local mvp = net.ReadEntity()
-    -- local mvp_damage = net.ReadUInt(32)
-    -- local mvp_kills = net.ReadUInt(32)
+--     -- local mvp = net.ReadEntity()
+--     -- local mvp_damage = net.ReadUInt(32)
+--     -- local mvp_kills = net.ReadUInt(32)
 
-    -- local damage_player = net.ReadEntity()
-    -- local most_damage = net.ReadUInt(32)
+--     -- local damage_player = net.ReadEntity()
+--     -- local most_damage = net.ReadUInt(32)
 
-    -- local kills_player = net.ReadEntity()
-    -- local most_kills = net.ReadUInt(32)
+--     -- local kills_player = net.ReadEntity()
+--     -- local most_kills = net.ReadUInt(32)
 
-    -- local most_heal_player = net.ReadEntity()
-    -- local most_heal = net.ReadUInt(32)
+--     -- local most_heal_player = net.ReadEntity()
+--     -- local most_heal = net.ReadUInt(32)
 
-    -- local headshot_player = net.ReadEntity()
-    -- local most_headshots = net.ReadUInt(32)
+--     -- local headshot_player = net.ReadEntity()
+--     -- local most_headshots = net.ReadUInt(32)
 
-    -- local elite_kill_player = net.ReadEntity()
-    -- local most_elite_kills = net.ReadUInt(32)
+--     -- local elite_kill_player = net.ReadEntity()
+--     -- local most_elite_kills = net.ReadUInt(32)
 
-    -- local damage_taken_player = net.ReadEntity()
-    -- local most_damage_taken = net.ReadUInt(32)
+--     -- local damage_taken_player = net.ReadEntity()
+--     -- local most_damage_taken = net.ReadUInt(32)
 
-    -- local total_damage = net.ReadUInt(32)
+--     -- local total_damage = net.ReadUInt(32)
 
-    -- local maps = net.ReadTable()
+--     -- local maps = net.ReadTable()
 
-    RTV.frame = vgui.Create('DFrame')
-    RTV.frame:SetTitle('')
-    RTV.frame:SetSize(s(1100), s(900))
-    RTV.frame:Center()
-    RTV.frame:SetMouseInputEnabled(true)
+--     RTV.frame = vgui.Create('DFrame')
+--     RTV.frame:SetTitle('')
+--     RTV.frame:SetSize(s(1100), s(900))
+--     RTV.frame:Center()
+--     RTV.frame:SetMouseInputEnabled(true)
     
-    local dsheet = RTV.frame:Add('DPropertySheet')
-    dsheet:Dock(FILL)
+--     local dsheet = RTV.frame:Add('DPropertySheet')
+--     dsheet:Dock(FILL)
 
-    local lboard = dseet:Add('Panel')
-    dsheet:AddSheet(translate.Get('Game_Game_Summary'), lboard, 'icon16/gun.png')
-end)
+--     local lboard = dseet:Add('Panel')
+--     dsheet:AddSheet(translate.Get('Game_Game_Summary'), lboard, 'icon16/gun.png')
+-- end)
 
-RTV.create_player_panel = function (pos, ply, award, reason)
-    local panel = vgui.Create("DPanel", summary_panel)
-    -- panel:Dock(TOP)
+local player_panel_h = s(150)
+local w = s(1000)
+local h = s(700)
+RTV.create_player_panel = function (pnl, pos, ply, award, reason)
+    local panel = vgui.Create("DPanel", pnl)
     panel:DockPadding(10, 10, 10, 10)
     panel:SetSize(w, player_panel_h)
     panel:SetPos(pos.x, pos.y)
     panel:SetBackgroundColor(HORDE.color_hollow)
     panel.Paint = function ()
-        if award == "MVP" then
-            draw.RoundedBox(10, 0, 0, w / 2, player_panel_h, HORDE.color_crimson)
-        else
-            --draw.RoundedBox(10, 0, 0, w / 2, player_panel_h, Color(100,100,100))
-        end
-        draw.RoundedBox(10, 2, 2, w / 2 - 4, player_panel_h - 4, Color(40,40,40))
+        draw.RoundedBox(10, 2, 2, w / 2 - 4, player_panel_h - 4, colors.hvr)
     end
     local avatar = vgui.Create("AvatarImage", panel)
     avatar:Dock(LEFT)
@@ -164,7 +161,7 @@ concommand.Add('shizlib_rtv_menu_test', function()
     }
 
     RTV.frame = vgui.Create('DPanel')
-    RTV.frame:SetSize(s(1000), s(700))
+    RTV.frame:SetSize(s(1015), s(800))
     RTV.frame:Center()
     RTV.frame:MakePopup()
     local keypressed = false
@@ -188,14 +185,9 @@ concommand.Add('shizlib_rtv_menu_test', function()
         draw.SimpleText(translate.Get("Game_Result_" .. status) .. "! " .. game.GetMap() .. " - " .. translate.Get("Game_Difficulty_" .. HORDE.difficulty_text[HORDE.difficulty]), 'font.40', w/2, s(40), color_white, 1, 1)
     end
 
-    local mvp_pnl = lboard:Add('Panel')
-    mvp_pnl:SetSize(s(700), s(150))
-    mvp_pnl:SetPos(s(150), s(80))
-
-    local mvp_avatar = mvp_pnl:Add('AvatarImage')
-    mvp_avatar:SetPlayer(mvp, 256)
-    mvp_avatar:SetSize(s(140), s(140))
-    mvp_avatar:SetPos(s(5), s(5))
+    RTV.create_player_panel(lboard, {x=w/4, y=s(80)}, mvp, 'MVP', tostring(mvp_kills) .. " " .. translate.Get("Game_Kills") .. ", " .. tostring(mvp_damage) .. " " .. translate.Get("Game_Damage") .. " (" .. tostring(percentage) .. "%)")
+    RTV.create_player_panel(lboard, {x=0, y=s(90) + player_panel_h}, damage_player, translate.Get("Game_Most_Damage_Dealt"), tostring(most_damage) .. " " .. translate.Get("Game_Damage"))
+    RTV.create_player_panel(lboard, {x=w/2, y=s(90) + player_panel_h}, kills_player, translate.Get("Game_Most_Kills"), tostring(most_kills) .. " " .. translate.Get("Game_Kills"))
 
     dsheet:AddSheet(translate.Get('Game_Game_Summary'), lboard, 'icon16/gun.png')
 end)
