9407e5e775732f9e20afdabd49e36028d96e934c
kasan0v <naddition42@gmail.com>
2024-10-12 18:58:36 +0000

    fix: небольшие изменения в чате

--- addons/shiz-addons/lua/shizlib-addons/chat/chat_cl.lua
+++ addons/shiz-addons/lua/shizlib-addons/chat/chat_cl.lua
@@ -1,9 +1,3 @@
--- CHAT = CHAT or {}
-
--- if not _chatAddText then
---     _chatAddText = chat.AddText
--- end
-
 local s = shizlib.surface.s
 local DTR = shizlib.surface.DTR
 local colors = CFG.skinColors
@@ -12,223 +6,6 @@ local draw = CLIENT and draw
 local draw_RoundedBox = CLIENT and draw.RoundedBox
 local draw_RoundedBoxEx = CLIENT and draw.RoundedBoxEx
 local draw_SimpleText = CLIENT and draw.SimpleText
-
--- function CHAT.Init()
---     if IsValid(CHAT.freak) or IsValid(CHAT.fr) then
---         CHAT.freak:Remove()
---         CHAT.fr:Remove()
---     end
---     hook.Add('HUDPaint', 'shizlib-chatPaint', function()
---         local w = s(45)
---         local h = s(640)
---         local x = s(100)
---         local y = s(300)
-
---         if CHAT.fr:IsVisible() then
---             CHAT.hud_richText:SetPos(9999, 9999)
---         else
---             CHAT.hud_richText:Dock(FILL)
---             CHAT.hud_richText:DockMargin(s(5), s(5), s(5), s(40))
---         end
---     end)
-
---     CHAT.freak = vgui.Create('DPanel')
---     CHAT.freak:SetPos(s(100), s(640))
---     CHAT.freak:SetSize(s(600), s(300))
---     function CHAT.freak:Paint(w, h)
---         draw.RoundedBox(0, 0, 0, 0, 0, color_white)
---     end
-
---     CHAT.fr = vgui.Create('DFrame')
---     CHAT.fr:SetVisible(false)
---     CHAT.fr:SetPos(s(100), s(640))
---     CHAT.fr:SetSize(s(600), s(300))
---     CHAT.fr:ShowCloseButton(false)
---     CHAT.fr:SetDraggable(true)
---     CHAT.fr:SetTitle('Pretty Chating')
-    
---     local keypressed = true
---     function CHAT.fr:Think()
---         if input.IsButtonDown(KEY_ESCAPE) then
---             keypressed = true
---         else
---             keypressed = false
---         end
---         if input.IsButtonDown(KEY_ESCAPE) and keypressed then
---             CHAT.ChangeVisible(false)
---             gui.HideGameUI()
---         end
---     end
-
---     CHAT.richText = CHAT.fr:Add('RichText')
---     CHAT.richText:Dock(FILL)
---     CHAT.richText:DockMargin(s(5), s(5), s(5), s(40))
---     CHAT.richText:SetVerticalScrollbarEnabled(true)
---     function CHAT.richText:Paint(w, h)
---         self:DrawTextEntryText(Color(25, 25, 25), Color(25, 25, 25), Color(25, 25, 25))
---         self:SetFontInternal('font.20')
---     end
-
---     CHAT.hud_richText = CHAT.freak:Add('RichText')
---     CHAT.hud_richText:Dock(FILL)
---     CHAT.hud_richText:DockMargin(s(5), s(5), s(5), s(40))
---     CHAT.hud_richText:SetVerticalScrollbarEnabled(false)
---     function CHAT.hud_richText:Paint(w, h)
---         self:DrawTextEntryText(Color(25, 25, 25), Color(25, 25, 25), Color(25, 25, 25))
---         self:SetFontInternal('font.20')
---     end
-
---     CHAT.textBox = CHAT.fr:Add('DTextEntry')
---     CHAT.textBox:SetPos(s(5), CHAT.fr:GetTall() - s(35))
---     CHAT.textBox:SetSize(CHAT.fr:GetWide() - s(10), s(30))
---     CHAT.textBox:SetTextColor(color_black)
-
---     function CHAT.textBox:OnKeyCodeTyped(code)
---         if code == KEY_ESCAPE then
---             CHAT.ChangeVisible(false)
---             gui.HideGameUI()
---         elseif code == KEY_ENTER then
-
---             if string.Trim( self:GetText() ) != '' then
---                 LocalPlayer():ConCommand( [[say ]] .. self:GetText() )
---             end
---             self:SetText('')
-
---             CHAT.ChangeVisible(false)
---         elseif code == KEY_TAB then
---             self:SetText( autotypePlayerName( self:GetText() ) );
-
---             timer.Simple(.002, function() self:RequestFocus() end)
---         end
---     end
---     function CHAT.textBox:OnTextChanged()
---         if self and self.GetText then 
--- 			gamemode.Call('ChatTextChanged', self:GetText() or '')
--- 		end
---     end
--- end
-
--- function autotypePlayerName( str )
---     local LastWord = string.match( str, '%a+$' );
-
---     if (LastWord == nil) then
---         return str;
---     end
-
---     playerlist = player.GetAll()
---     for k, v in pairs( playerlist ) do
---         local nickname = v:Nick()
---         if string.len(LastWord) < string.len(nickname) && string.find( string.lower(nickname), string.lower(LastWord) ) == 1 then
---             str = string.sub( str, 1, (string.len(LastWord) * -1) - 1)
---             str = str .. nickname
---             return str
---         end
---     end
-
---     return str;
--- end
-
--- function CHAT.ChangeVisible( bool )
---     if not IsValid(CHAT.fr) or not IsValid(CHAT.freak) then CHAT.Init() end
-
---     CHAT.fr:SetMouseInputEnabled(bool)
---     CHAT.fr:SetKeyboardInputEnabled(bool)
---     gui.EnableScreenClicker(bool)
---     CHAT.fr:SetVerticalScrollbarEnabled(true)
---     CHAT.fr:SetVisible(bool)
-
---     if bool then
---         CHAT.fr:MakePopup()
---         CHAT.textBox:RequestFocus()
---         CHAT.textBox:SetText('')
-
---         gamemode.Call('StartChat')
-
---     end
--- end
-
--- function appendToChat(obj)
---     if not IsValid(CHAT.richText) or not IsValid(CHAT.hud_richText) then return end
---     if type(obj) == 'table' then
---         CHAT.richText:InsertColorChange( obj.r, obj.g, obj.b, 255 )
---         CHAT.hud_richText:InsertColorChange( obj.r, obj.g, obj.b, 255 )
---     elseif type(obj) == 'string' then
---         CHAT.richText:AppendText(obj)
---         CHAT.hud_richText:AppendText(obj)
---         CHAT.hud_richText:InsertFade(10, 2.5)
---     end
--- end
-
--- function chat.AddText(...)
---     local args = {...}
-    
---     appendToChat('\n');
-
---     for _, obj in pairs( args ) do
---         if type(obj) == 'table' or type(obj) == 'string' then
---             appendToChat(obj);
---         elseif obj:IsPlayer() then
---             local col = GAMEMODE:GetTeamColor(obj);
---             appendToChat(col);
---             appendToChat(obj:Nick());
---         end
---     end
-
---     _chatAddText(...);
-    
--- end
-
--- hook.Add('InitPostEntity', 'shizlib-chatInit', function()
---     timer.Simple(1, function()
---         CHAT.Init()
---     end)
--- end)
-
--- hook.Add('PlayerBindPress', 'overrideChatbind', function( ply, bind, pressed )
---     if bind ~= 'messagemode' and bind ~= 'messagemode2' then return end
-
---     CHAT.ChangeVisible(true)
---     return true
--- end)
-
--- hook.Add('HUDShouldDraw', 'noMoreDefault', function(name)
---     if name == 'CHudChat' then return false end
--- end)
-
--- hook.Add('ChatText', 'serverNotifications', function(index, name, text, type)
--- 	if type == 'joinleave' or type == 'none' then
--- 		appendToChat('\n' .. text)
--- 	end
--- end)
-
--- concommand.Add('shizlib_reloadchat', function()
---     CHAT.Init()
--- end)
-
-f4 = f4 or {}
-f4.Colors = {
-	background 		= Color(48,50,65,250),
-	button_tab 		= Color(117, 156, 169, 160),
-	background_hud 	= Color(107, 115, 118, 100),
-	shop 			= Color(48,50,65,125),
-	job 			= Color(161,104,255,5),
-	panel			= Color(107, 115, 118, 100),
-	top				= Color(38, 38, 38, 200),
-	top_grad		= Color(38, 38, 38, 200),
-	button			= Color(38, 38, 38, 200),
-	close			= Color(239,231,255),
-	close_hover		= Color(48,50,65),
-	max				= Color(175,175,175),
-	vip				= Color(255,208,108),
-	battlepass		= Color(152,108,255),
-	green			= Color(123,223,77),
-	red				= Color(199,28,28),
-	blue			= Color(28,102,199),
-	orange			= Color(255,170,73),
-	root 			= Color(255,0,0),
-	license			= Color(147,107,27,200),
-	arrest 			= Color(255,99,99),
-}
 
 local pmeta = FindMetaTable('Player')
 
@@ -394,7 +171,7 @@ local patterns = {
             return {
                 data = str,
                 type = 'link',
-                color = Color(0, 0, 255),
+                color = colors.bg,
                 underline = true
             }
         end
@@ -1080,7 +857,7 @@ function PANEL:Init()
     local send = Material('shizlib_chat/send.png', 'smooth')
 
     self.Send.Paint = function(s, w, h)
-        local col = s.Hovered and color_white or f4.Colors.top
+        local col = s.Hovered and color_white or colors.hvr
         surface.SetMaterial(send)
         surface.SetDrawColor(col)
         surface.DrawTexturedRect(w/2-p(10), h/2-p(10), p(20), p(20))
@@ -1109,7 +886,7 @@ function PANEL:Init()
 
     self.Scroll.VBar.btnGrip.Paint = function(s, w, h)
         if self:IsKeyboardInputEnabled() then
-            draw.RoundedBox(shizlib.surface.s(8),0,0,w,h,f4.Colors.top_grad)
+            draw.RoundedBox(shizlib.surface.s(8),0,0,w,h,colors.hvr)
         end
     end
 
@@ -1210,8 +987,6 @@ hook.Add('PlayerBindPress', 'shizlib.PlayerBindPress', function(ply, bind, press
 end)
 
 function shizlib.openChatbox(bTeam)
-    if LocalPlayer().ShowF4 or LocalPlayer().ShowTabs then return end
-
     shizlib.ChatBox.Team = bTeam
     shizlib.ChatBox:MakePopup()
     shizlib.ChatBox.TextEntry:RequestFocus()
