485a88b2743854025a3a83ad19e93c19dca9f4ac
kasan0v <naddition42@gmail.com>
2024-10-13 15:00:03 +0000

    test

--- gamemodes/horde/entities/entities/projectile_horde_ar2_projectile/shared.lua
+++ gamemodes/horde/entities/entities/projectile_horde_ar2_projectile/shared.lua
@@ -19,7 +19,6 @@ ENT.BaseDamage = 25
 ENT.BaseSplashDamage = 10
 ENT.DirectDamage = 60            -- How much damage should it do when it hits something
 ENT.DirectDamageType = DMG_CRUSH -- Damage type
-ENT.BounceCount = 0
 
 function ENT:Draw()
 	self:DrawModel()
@@ -64,14 +63,8 @@ function ENT:SetupDataTables()
 	self:NetworkVar("Int", 0, "Charged")
 end
 
-local MAX_BOUNCE = 5
-
 function ENT:OnBounce(data, phys)
-	if (self.BounceCount >= MAX_BOUNCE) then
-		self:Remove()
-	else 
-		self.BounceCount = self.BounceCount + 1
-	end
+
 end
 
 function ENT:PhysicsCollide(data, phys)
