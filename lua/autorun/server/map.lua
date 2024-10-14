e6468d17f3e8eded7dba408fd9ee84027b6b81dd
kasan0v <naddition42@gmail.com>
2024-10-14 13:55:30 +0000

    add: новые карты + контент в workshop.lua

--- lua/autorun/server/map.lua
+++ lua/autorun/server/map.lua
@@ -9,6 +9,10 @@ maplist["hr_zombiealley"] = "2840619224"
 maplist["hr_dust2"] = "2638826810"
 maplist["hr_rooftop_v1_fixed"] = "2930690532"
 maplist["de_stad"] = "1751765989"
+maplist["hr_castle_keep"] = "2974218085"
+maplist["hr_snowedin"] = "2987982745"
+maplist["hr_abandoned_mall_v2_fixed"] = "2925321027"
+maplist["hr_the_zombie_pit"] = "2741139654"
 
 
 local map = game.GetMap() local workshopid = maplist[map] if(workshopid != nil) then resource.AddWorkshop(workshopid) end
