7729fa38a37ed4048c5c7478fe49272d5ade62fc
kasan0v <naddition42@gmail.com>
2024-11-15 08:47:35 +0000

    fix: делаем виньетку менее резкой

--- addons/shiz-addons/lua/shizlib-addons/interface/visual_cl.lua
+++ addons/shiz-addons/lua/shizlib-addons/interface/visual_cl.lua
@@ -5,10 +5,10 @@ CreateClientConVar('shizlib_hud_vingette', 0, true, true)
 
 hook.Add('HUDPaint', 'shizlib-vignette', function()
     if GetConVar('shizlib_hud_vingette'):GetInt() == 0 then return end
-    DTR(0, 0, s(1920), s(500), Color(0,0,0, 225), Material('vgui/gradient-u'))
-    DTR(0, s(580), s(1920), s(500), Color(0,0,0, 225), Material('vgui/gradient-d'))
-    DTR(s(1420), 0, s(500), s(1080), Color(0,0,0, 225), Material('vgui/gradient-r'))
-    DTR(0, 0, s(500), s(1080), Color(0,0,0, 225), Material('vgui/gradient-l'))
+    DTR(0, 0, s(1920), s(300), Color(0,0,0, 225), Material('vgui/gradient-u'))
+    DTR(0, s(880), s(1920), s(300), Color(0,0,0, 225), Material('vgui/gradient-d'))
+    DTR(s(1620), 0, s(300), s(1080), Color(0,0,0, 225), Material('vgui/gradient-r'))
+    DTR(0, 0, s(300), s(1080), Color(0,0,0, 225), Material('vgui/gradient-l'))
 end)
 
 hook.Add('SpawnMenuOpen', 'shizlib-QMenu', function()
